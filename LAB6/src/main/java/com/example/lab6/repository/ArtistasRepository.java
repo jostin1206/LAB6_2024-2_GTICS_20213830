c561173f219b7e15e63507096911f713c6f43fec
jostin1206 <a20213830@pucp.edu.pe>
2024-10-02 21:19:07 -0500

    lab6-artistas

--- LAB6/src/main/java/com/example/lab6/repository/ArtistasRepository.java
+++ LAB6/src/main/java/com/example/lab6/repository/ArtistasRepository.java
@@ -1,9 +1,10 @@
 package com.example.lab6.repository;
 
+import com.example.lab6.entity.Artistas;
 import com.example.lab6.entity.Eventos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArtistasRepository extends JpaRepository<Eventos, Integer> {
+public interface ArtistasRepository extends JpaRepository<Artistas, Integer> {
 }
